683bf2fa0e01ad4db5cedf8123add6f6fa2a7a35
Crowdbotics <team@crowdbotics.com>
2019-04-06 21:18:23 +0000

    initial commit

---
+++ android/app/src/main/java/com/baiano_2027/MainActivity.java
@@ -0,0 +1,15 @@
+package com.baiano_2027;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "baiano_2027";
+    }
+}
